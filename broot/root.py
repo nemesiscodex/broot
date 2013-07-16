2afdea55a75dc21780727032486e03a4e687362f
Daniel Narvaez <dwnarvaez@gmail.com>
2013-07-16 23:16:05 +0200

    Create the shm directory

--- broot/root.py
+++ broot/root.py
@@ -369,6 +369,7 @@ class Root:
 
         try:
             os.makedirs(os.path.join(self.path, "var/run/dbus"))
+            os.makedirs(os.path.join(self.path, "dev/shm"))
         except OSError:
             pass
 
