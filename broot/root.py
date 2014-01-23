2e4495f123ed5ce39c8d57122798f2c6a4fb05fe
Daniel Narvaez <dwnarvaez@gmail.com>
2014-01-24 00:41:51 +0100

    Use loglevel error for npm

--- broot/root.py
+++ broot/root.py
@@ -164,7 +164,8 @@ class Root:
     def _install_npm_packages(self):
         npm_packages = self._config.get("npm_packages")
         if npm_packages:
-            self.run("npm install -q -g %s" % " ".join(npm_packages),
+            self.run("npm install --loglevel error -g %s" %
+                     " ".join(npm_packages),
                      as_root=True)
 
     def _install_os_packages(self):
